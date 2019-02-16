b52e32669c2668be51de7063be33db1a233e1fa9
JuanCamiloEcheverry <47701425+JuanCamiloEcheverry@users.noreply.github.com>
2019-02-16 17:24:09 -0500

    1859856

---
+++ 1859856/calculadora.h
@@ -0,0 +1,21 @@
+#indef__CALCULADORA__H
+#define__CALCULADORA__H
+
+class calculadora {
+	
+	float suma
+	float resta
+	float multiplicacion
+	float division
+	
+	
+};
+
+    float suma();
+	float resta();
+	float multiplicacion;
+	float division;
+	
+	
+#indef
+	
