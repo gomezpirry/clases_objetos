b78ccb8cc4e90c8a7849fa8eeaae669458772882
JuanCamiloEcheverry <47701425+JuanCamiloEcheverry@users.noreply.github.com>
2019-02-16 17:19:04 -0500

    1859856

---
+++ 1859856/main.cpp
@@ -0,0 +1,19 @@
+#include 
+ 
+  int main(int argc, char* argv[]) 
+  
+  suma (num1,num2) 
+  { num1+num2
+  };
+  
+   resta (num1,num2) 
+  { num1-num2
+  };
+  
+   multiplicacion (num1,num2) 
+  { num1*num2
+  };
+  
+   division (num1,num2) 
+  { num1/num2
+  };
