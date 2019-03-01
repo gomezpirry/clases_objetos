09acec2de30d08c1f1593448e2ceb280c6048b80
bustosgaviria <48114035+bustosgaviria@users.noreply.github.com>
2019-03-01 18:36:09 -0500

    Add files via upload

---
+++ 1859997-1860021-1859905/Calculadora/calculadora.cpp
@@ -0,0 +1,23 @@
+#include "calculadora.h"
+
+    float calculadora::sumar(){
+	float resultado ;
+	resultado= valor1 + valor2 ;
+	return resultado;}
+	
+	
+	float calculadora::restar(){
+	float resultado ;
+	resultado= valor1 - valor2 ;
+	return resultado;}
+	
+	
+	float calculadora::multiplicar(){
+	float resultado ;
+	resultado= valor1 * valor2 ;
+	return resultado;}
+	
+	float calculadora::dividir(){
+	float resultado ;
+	resultado= valor1 / valor2 ;
+	return resultado;}
