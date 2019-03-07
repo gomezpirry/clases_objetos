a5ca1459c7bb4086eed3ae60d2d2b71e8ec7957a
jdchavarro <jd-chavarro@hotmail.com>
2019-03-06 23:24:42 -0500

    correcion errores

--- 1866205-1860321-1860255/consultaUsuario/Curso.h
+++ 1866205-1860321-1860255/consultaUsuario/Curso.h
@@ -1,8 +1,8 @@
 #ifndef CURSO_H
 #define CURSO_H
-#include "Profesor.h"
-#include "Estudiante.h"
+
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -10,8 +10,6 @@ class Curso {
 private:
 	int codigo;
 	string nombre;
-	vector<Estudiante>* estudiantes;
-	Profesor profe;
 public:
 	Curso();
 	~Curso();
@@ -30,14 +28,6 @@ public:
 	
 	string getNombre(){
 		return nombre;
-	}
-	
-	void setEstudiantes(vector<Estudiante>* estudiantes){
-		this->estudiantes = estudiantes;
-	}
-	
-	void setProfesor(Profesor profe){
-		this->profe = profe;
 	}
 };
 
