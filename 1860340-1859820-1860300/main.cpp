22803505c210d56721a72f43799d658748fdccd7
Juan Pablo Millan <47467105+JuanMillan7818@users.noreply.github.com>
2019-03-15 13:41:48 -0500

    1860340-1859820-1860300

---
+++ 1860340-1859820-1860300/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdlib.h>
+#include "Laboratorio.h"
+
+using namespace std ;
+
+int main() {
+	int a = 3;
+	Laboratorio Inicio ;
+	Inicio.InicioPrograma() ;
+	
+	system("pause") ;
+	return 0;
+}
